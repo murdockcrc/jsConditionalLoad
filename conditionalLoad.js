fd0b4e67ea3aa74353ed40d6981cea11e2c48d1f
murdockcrc <luis@delgado.ms>
2014-10-15 13:35:24 +0200

    Update conditionalLoad.js

--- conditionalLoad.js
+++ conditionalLoad.js
@@ -1,7 +1,8 @@
 //Conditional loading of Intl.js, as Safari DOES NOT include this library
 $(function () {
     if (!window.Intl) {
-        $.getScript('/js/localization/Intl/Intl.min.js');
+        $.getScript('/js/localization/Intl/Intl.min.js'); //Path to Intl.min.js file
+        //Include localization files that are needed. In this case, I am including German localization files. These are included in Intl.js source code.
         $.getScript('/js/localization/Intl/de.js');
         $.getScript('/js/localization/Intl/de-DE.js');
     }
